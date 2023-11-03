89cb1ae344d30fdedfcd77386568ce9e54a157a7
Maryia Tsvilik <mtsvilik@solvd.com>
2023-11-03 15:38:05 +0300

    Test was updated

--- src/test/java/com/solvd/saucedemocarina/SaucedemoTest.java
+++ src/test/java/com/solvd/saucedemocarina/SaucedemoTest.java
@@ -3,19 +3,16 @@ package com.solvd.saucedemocarina;
 import com.solvd.saucedemocarina.pages.LoginPage;
 import com.solvd.saucedemocarina.pages.ProductsPage;
 import com.solvd.saucedemocarina.pages.ShoppingCartPage;
-import com.solvd.saucedemocarina.utils.CapabilityFactory;
 import com.zebrunner.carina.core.IAbstractTest;
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
 public class SaucedemoTest implements IAbstractTest {
 
     @Test
-    @Parameters({"browserName"})
-    public void checkAddProductToCartTest(String browserName) {
-        LoginPage loginPage = new LoginPage(getDriver(browserName, CapabilityFactory.createCapabilities(browserName)));
+    public void checkAddProductToCartTest() {
+        LoginPage loginPage = new LoginPage(getDriver());
         loginPage.open();
         Assert.assertTrue(loginPage.isPageOpened());
 
@@ -26,7 +23,6 @@ public class SaucedemoTest implements IAbstractTest {
         ShoppingCartPage shoppingCartPage = productsPage.goToShoppingCart();
         String expectedProductTitle = shoppingCartPage.getProductTitle();
 
-        Assert.assertEquals(actualProductTitle, expectedProductTitle,
-                "Product title is incorrect");
+        Assert.assertEquals(actualProductTitle, expectedProductTitle, "Product title is incorrect");
     }
 }
