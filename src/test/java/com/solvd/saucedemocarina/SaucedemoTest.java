1ad13784b8e3dd1b0b09a28bcee35438ac67478c
Maryia Tsvilik <mtsvilik@solvd.com>
2023-11-03 15:52:20 +0300

    Test for zebrunner

--- src/test/java/com/solvd/saucedemocarina/SaucedemoTest.java
+++ src/test/java/com/solvd/saucedemocarina/SaucedemoTest.java
@@ -3,16 +3,19 @@ package com.solvd.saucedemocarina;
 import com.solvd.saucedemocarina.pages.LoginPage;
 import com.solvd.saucedemocarina.pages.ProductsPage;
 import com.solvd.saucedemocarina.pages.ShoppingCartPage;
+import com.solvd.saucedemocarina.utils.CapabilityFactory;
 import com.zebrunner.carina.core.IAbstractTest;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
 public class SaucedemoTest implements IAbstractTest {
 
     @Test
-    public void checkAddProductToCartTest() {
-        LoginPage loginPage = new LoginPage(getDriver());
+    @Parameters({"browserName"})
+    public void checkAddProductToCartTest(String browserName) {
+        LoginPage loginPage = new LoginPage(getDriver(browserName, CapabilityFactory.createCapabilities(browserName)));
         loginPage.open();
         Assert.assertTrue(loginPage.isPageOpened());
 
